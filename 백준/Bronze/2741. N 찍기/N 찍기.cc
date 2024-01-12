8d5b1e13c44824e72611359478afd8cb99fc344a
expr <155818689+expr01@users.noreply.github.com>
2024-01-12 17:59:08 +0800

    [Bronze V] Title: N 찍기, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    int n = 0;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cout << i + 1 << "\n";
+    }
+
+    return 0;
+}   
