82aba6d2afd82a34f316ddc43dc16bfdfadb58df
expr <155818689+expr01@users.noreply.github.com>
2024-01-21 17:39:39 +0800

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    char inputWord;
+    cin >> inputWord;
+    int asciiValue = int(inputWord);
+
+    cout << asciiValue;
+    return 0;
+}   
