76b15f6c27ef573ded399ec15e6eb363c8957aa5
expr <155818689+expr01@users.noreply.github.com>
2024-01-21 17:36:00 +0800

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main () {
+
+    string inputStr;
+    int i;
+    cin >> inputStr;
+    cin >> i;
+    cout << inputStr[i - 1];
+
+    return 0;
+}   
