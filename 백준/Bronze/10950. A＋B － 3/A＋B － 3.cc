56fb8f87d351b539a8e19585655f2b9c3767b9f1
expr <155818689+expr01@users.noreply.github.com>
2024-01-24 19:02:39 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    int testCase;
+    cin >> testCase;
+
+    for (int i = 0; i < testCase; i++) {
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << "\n";
+    }
+
+    return 0;
+}   
