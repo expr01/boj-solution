9f888222a64be0d5893c844eaf2b893ef10ef503
expr <155818689+expr01@users.noreply.github.com>
2024-03-16 02:40:59 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    string word;
+
+    while (getline(cin, word)) {
+    cout << word << "\n";    
+    }
+
+    return 0;
+}
