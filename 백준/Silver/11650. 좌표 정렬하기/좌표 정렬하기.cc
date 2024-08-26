3a3666db04fcc7304e3cb786132f62d5e3b81d45
expr <155818689+expr01@users.noreply.github.com>
2024-08-26 22:12:32 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 40 ms, Memory: 2676 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+struct Coord {
+    int x; // x 좌표
+    int y; // y 좌표
+};
+
+// 비교 함수
+bool compare(const Coord &a, const Coord &b) {
+    if (a.x == b.x) {
+        return a.y < b.y; // x가 같으면 y로 정렬
+    }
+    return a.x < b.x; // x로 정렬
+}
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n; 
+    cin >> n;
+
+    // 좌표 구조체 배열 선언
+    Coord coord[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> coord[i].x >> coord[i].y;
+    }
+
+    sort(coord, coord + n, compare);
+    
+    for (int i = 0; i < n; i++) {
+        cout << coord[i].x << " " << coord[i].y << "\n";
+    }
+
+    return 0;
+}
