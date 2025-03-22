a49c08b684d5030fdbee8d5d7f0a18d90e958f2a
expr <155818689+expr01@users.noreply.github.com>
2025-03-22 19:14:13 +0900

    [level 0] Title: 2의 영역, Time: 5.78 ms, Memory: 15 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.py
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.py
@@ -1,5 +1,9 @@
 def solution(arr):
-    index = [i for i, value in enumerate(arr) if value == 2]
+    index = []
+    for i in range(len(arr)):
+        if arr[i] == 2:
+            index.append(i)
+    
     if not index:
         return [-1]
     return arr[index[0]:index[-1]+1]
