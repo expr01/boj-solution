c83d7f08e5d110c69ed98c27421f8600fefd651d
expr <155818689+expr01@users.noreply.github.com>
2025-06-25 10:42:12 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 4.11 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.cpp
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.cpp
@@ -6,22 +6,14 @@ using namespace std;
 // 장군개미: 5, 병정개미: 3, 일개미: 1
 int solution(int hp) {
     int count = 0;
-    while (hp > 0) {
-        if (hp >= 5) {
-            hp -= 5;
-            count++;
-            continue;
-        }
-        if (hp >= 3) {
-            hp -= 3;
-            count++;
-            continue;
-        }
-        if (hp >= 1) {
-            hp -= 1;
-            count++;
-            continue;
-        }
-    }
+    
+    count += hp / 5;
+    hp %= 5;
+
+    count += hp / 3;
+    hp %= 3;
+
+    count += hp / 1;
+
     return count;
 }
