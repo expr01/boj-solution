822931e3f8591ee101dffb5b72871ee41275e16d
expr <155818689+expr01@users.noreply.github.com>
2025-06-25 10:33:36 +0900

    [level 0] Title: 순서쌍의 개수, Time: 2.71 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    vector<int> divisor;
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) divisor.push_back(i);
+    }
+    return divisor.size();
+}
